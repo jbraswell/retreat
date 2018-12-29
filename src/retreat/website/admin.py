208eeea6b24ffe72706e3cf7d898af35255da223
Adam <36554147+ANichols4444@users.noreply.github.com>
2018-12-29 17:30:44 -0500

    Added attendee model

--- src/retreat/website/admin.py
+++ src/retreat/website/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Attendee
 
-# Register your models here.
+
+class AttendeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'amount_paid', 'payment_type', 'creation_date')
+
+
+admin.site.register(Attendee, AttendeeAdmin)
--- src/retreat/website/models.py
+++ src/retreat/website/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Attendee(models.Model):
+    PAYPAL = 'PP'
+    CASH = 'CA'
+    PAYMENT_TYPE_CHOICES = (
+        (PAYPAL, 'Paypal'),
+        (CASH, 'Cash')
+    )
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=True)
+    amount_paid = models.DecimalField(decimal_places=2, max_digits=10)
+    phone = models.CharField(null=True, max_length=255)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    payment_type = models.CharField(
+        max_length=2,
+        choices=PAYMENT_TYPE_CHOICES,
+        default=PAYPAL
+    )
+
+    def __str__(self):
+        return self.name
+
+
